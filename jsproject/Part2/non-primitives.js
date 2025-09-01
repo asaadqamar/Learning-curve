a00b27f6c406da06554db10655feabfa6a6fc9cc
asaadqamar <asaadqamar641@gmail.com>
2025-09-01 12:48:55 +0500

    add js part 2 code

---
+++ jsproject/Part2/non-primitives.js
@@ -0,0 +1,24 @@
+// Non Primitive like Object
+let allEmployee = {
+  firstname: "Ali",
+  JoinDate: "22-03-2023",
+  ID: 278347834,
+};
+// console.log(typeof allEmployee);
+// console.log(allEmployee);
+
+const allSystems = {
+  "Mian moniter": "LCD",
+  keyboard: "logi Tech",
+  systemID: 278347834,
+};
+
+// console.log(typeof allSystems);
+// console.log(allSystems.keyboard, allSystems.systemID);
+// console.log(allSystems[("Mian moniter", "systemID")]);
+
+//Array
+const deptTwo = ["naiz", 3675, false];
+
+// console.log(typeof deptTwo);
+console.log(deptTwo[1], deptTwo[0]);
---
+++ jsproject/Part2/primitives.js
@@ -0,0 +1,30 @@
+//
+
+// NUmber
+let tLeaves = 30;
+console.log(typeof tLeaves);
+
+// Boolean
+
+let onLeave = false;
+console.log(typeof onLeave);
+
+// null & Undefined
+let name;
+console.log(typeof name);
+
+let hasWork = null;
+console.log(typeof hasWork);
+
+// String
+let employeeName = "Ali";
+let welcome = "Hello" + employeeName;
+let newWelcome = `Hello ${employeeName}`;
+
+console.log(newWelcome);
+
+// Synmbol
+
+let sm1 = Symbol("onLeave");
+let sm2 = Symbol("inOffice");
+console.log(typeof sm1);
