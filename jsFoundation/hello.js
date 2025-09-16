e18ef7785e54fbaa9fe9df7161757065b63471e9
asaadqamar <asaadqamar641@gmail.com>
2025-09-16 10:58:27 +0500

    add basic concepts into it

---
+++ jsFoundation/hello.js
@@ -0,0 +1,5 @@
+let mainArray = ["a", "b", "c", "d", "e", "f"];
+console.log(mainArray.length);
+
+let maxObject = { name: "Asad", mail: "asad@gmail.com", ID: 23343, Age: 23 };
+console.log(Object.keys(maxObject));
