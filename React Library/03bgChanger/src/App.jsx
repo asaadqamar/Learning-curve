1773f8b4d1b06b024c22c0e9eb0e2303cf5bb077
asaadqamar <asaadqamar641@gmail.com>
2025-10-03 17:01:21 +0500

    add complete bgChanger project

--- React Library/03bgChanger/src/App.jsx
+++ React Library/03bgChanger/src/App.jsx
@@ -4,11 +4,45 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("olive");
 
   return (
     <>
-      <h1 className="text-3xl ">test</h1>
+      <div
+        className="w-full h-screen flex flex-wrap flex-row items-end justify-center"
+        style={{ backgroundColor: color }}
+      >
+        <div className="bg-amber-50 w-fit rounded-md p-3 mb-3 h-fit ">
+          <button
+            className="p-3 text-white shadow-2xl rounded-md m-2 cursor-pointer"
+            style={{ backgroundColor: "green" }}
+            onClick={() => setColor("green")}
+          >
+            Green
+          </button>
+          <button
+            className="p-3 text-white shadow-2xl rounded-md m-2 cursor-pointer"
+            style={{ backgroundColor: "yellow" }}
+            onClick={() => setColor("yellow")}
+          >
+            Yellow
+          </button>
+          <button
+            className="p-3 text-white shadow-2xl rounded-md m-2 cursor-pointer"
+            style={{ backgroundColor: "pink " }}
+            onClick={() => setColor("pink")}
+          >
+            Pink
+          </button>
+          <button
+            className="p-3 text-black shadow-2xl rounded-md m-2 cursor-pointer"
+            style={{ backgroundColor: "grey " }}
+            onClick={() => setColor("olive")}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
     </>
   );
 }
