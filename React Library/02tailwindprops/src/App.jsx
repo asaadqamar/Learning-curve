32d7f238f92a1fe1d5387f0d0a8ef22011778e5a
asaadqamar <asaadqamar641@gmail.com>
2025-10-03 11:38:13 +0500

    add tailwind in new react project

---
+++ React Library/02tailwindprops/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl underline">Test tailwind</h1>
+    </>
+  );
+}
+
+export default App;
