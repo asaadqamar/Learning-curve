73d8e34ed26d1d651007d981deb31afdb5770a05
asaadqamar <asaadqamar641@gmail.com>
2025-10-08 12:31:14 +0500

    add password genrator project frontend

--- React Library/04passwordGenrator/src/App.jsx
+++ React Library/04passwordGenrator/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen bg-gray-800 flex justify-center align-middle items-center">
+        <div className="bg-white h-fit p-5 rounded-md">
+          <h1 className="text-3xl font-bold text-center">Password Genrator</h1>
+          <div className="flex p-5">
+            <input
+              className="border border-gray-500 p-2 w-90 focus:outline-none rounded-l-md"
+              type="text"
+              name="password"
+              id="password"
+              readOnly
+            />
+            <button
+              className="bg-blue-800 text-white p-2 rounded-r-md cursor-pointer"
+              type="button"
+            >
+              Copy
+            </button>
+          </div>
+          <div className="flex gap-5">
+            <div className="flex">
+              <label className="p-2" htmlFor="range">
+                Length
+              </label>
+              <input type="range" name="range" id="range" min="8" max="20" />
+            </div>
+            <div className="flex">
+              <label className="p-2" htmlFor="number">
+                Number
+              </label>
+              <input type="checkbox" name="number" id="number" />
+            </div>
+            <div className="flex">
+              <label className="p-2" htmlFor="char">
+                Special Charater
+              </label>
+              <input type="checkbox" name="char" id="char" />
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
