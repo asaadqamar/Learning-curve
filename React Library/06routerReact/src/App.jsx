c3ab606f35e40e81f4c6daae202df6a09ed250c7
asaadqamar <asaadqamar641@gmail.com>
2025-10-15 17:14:42 +0500

    add react router project

--- React Library/06routerReact/src/App.jsx
+++ React Library/06routerReact/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Layout from "./components/Layout";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import GitHub from "./components/GitHub/GitHub";
+import Contact from "./components/Contact/Contact";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="conatct" element={<Contact />} />
+      <Route path="github" element={<GitHub />} />
+      <Route path="*" element={<div> Not Found </div>} />
+    </Route>
+  )
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ React Library/06routerReact/src/components/GitHub/GitHub.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+function GitHub() {
+  const [data, setData] = React.useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/hiteshchoudhary`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+  return (
+    <>
+      <div className="m-5 py-5 bg-black text-white text-center text-3xl">
+        Followers {data.followers}
+        <img src={data.avatar_url} alt="" srcset="" />
+      </div>
+    </>
+  );
+}
+
+export default GitHub;
