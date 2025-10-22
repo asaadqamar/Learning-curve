fbc38ba07ac2c6b9935de6b6c6b316073b986ae4
asaadqamar <asaadqamar641@gmail.com>
2025-10-22 16:22:44 +0500

    add redux todo app

--- React Library/10ReduxToDo/src/App.jsx
+++ React Library/10ReduxToDo/src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import AddToDo from "./components/AddToDo";
+import ListToDo from "./components/ListToDo";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1 className="text-3xl bg-black text-white">
-        Redux Toolkit for ToDo in React
-      </h1>
-    </>
+    <Provider store={store}>
+      <AddToDo />
+      <ListToDo />
+    </Provider>
   );
 }
 
---
+++ React Library/10ReduxToDo/src/feature/todo/todoSlice.js
@@ -0,0 +1,26 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { act } from "react";
+
+const initialState = {
+  todos: [{ id: 1, text: "Hello World" }],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addToDo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeToDo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addToDo, removeToDo } = todoSlice.actions;
+export default todoSlice.reducer;
